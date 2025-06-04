5c5925c46076f84154e60d78cbd13bf1f6598c52
SeVer-14 <st_VershininSM@tomskasu.ru>
2025-06-04 12:53:16 +0700

    Add files via upload

---
+++ migrations/001_basics.sql
@@ -0,0 +1,30 @@
+-- +goose Up
+CREATE TABLE products
+(
+    id         SERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL UNIQUE,
+    title      TEXT   NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE carts
+(
+    id         SERIAL PRIMARY KEY,
+    user_id    BIGINT  NOT NULL,
+    product_id BIGINT  NOT NULL,
+    quantity   INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
+
+-- +goose Down
+DROP TABLE carts;
+DROP TABLE products;
+
+
+
+
